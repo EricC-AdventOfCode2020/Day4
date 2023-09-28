e9b965f9d894af8abef7937ab82b8739fa302677
BreadBurgIar <ericcom@outlook.com>
2023-09-27 23:43:51 -0400

    added initial files

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> data = new List<string>();
+
+            using (var sr = new StreamReader(@"..\..\..\input"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    if (line == "")
+                        line = "-";
+
+                    data.Add(line!);
+                }
+            }
+
+            // TODO: find way to split different batches (DONE)
+            // SOLUTION: made it so when reading file, if line == empty string, replace line with '-'
+
+            // TODO: solve problem now
+            for (int i = 0; i < data.Count; i++)
+            {
+
+            }
+        }
+    }
+}
