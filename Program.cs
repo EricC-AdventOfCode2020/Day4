01121a322d741334d3fd2b7d340929dd6a0caf71
Eric Comanescu <ericcom@outlook.com>
2023-10-03 18:52:21 -0400

    fuck

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Day4
 {
@@ -6,6 +7,23 @@ namespace Day4
     {
         static void Main(string[] args)
         {
+            List<string> fixedData = new List<string>()
+            {
+                "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+                "byr:1937 iyr:2017 cid:147 hgt:183cm",
+                "-",
+                "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
+                "hcl:#cfa07d byr:1929",
+                "-",
+                "hcl:#ae17e1 iyr:2013",
+                "eyr:2024",
+                "ecl:brn pid:760753108 byr:1931",
+                "hgt:179cm",
+                "-",
+                "hcl:#cfa07d eyr:2025 pid:166559648",
+                "iyr:2011 ecl:brn hgt:59in"
+            };
+
             List<string> data = new List<string>();
 
             using (var sr = new StreamReader(@"..\..\..\input"))
@@ -25,42 +43,50 @@ namespace Day4
             // SOLUTION: made it so when reading file, if line == empty string, replace line with '-'
 
             // TODO: solve problem now
-            string[] fields = { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "cid", "hgt" };
+            List<string> fields = new List<string>() { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt", "cid" };
+            List<string> fieldsCopy = new List<string>() { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt", "cid" };
 
             bool[] fieldsPresent = { false, false, false, false, false, false, false, false };
 
             int validCount = 0;
 
+            StringBuilder sb = new StringBuilder();
+
+            // im going to kill myself
             for (int i = 0; i < data.Count; i++)
             {
-                string line = data[i];
-
-                if (line == "-")
+                if (data[i] != "-")
                 {
-                    for (int j = 0; j < fields.Length; j++)
-                    {
-                        if (fieldsPresent[j] == false && fields[j] != "cid")
-                            break;
-                        else if (fieldsPresent[j] == true && j == fields.Length - 1)
-                            validCount++;
-                    }
+                    sb.Append(data[i]);
                 }
-                else
+
+                if (i + 1 < data.Count && data[i + 1] != "-" && data[i] != "-")
                 {
-                    string[] subStrings = line.Split(' ');
+                    sb.Append(" ");
+                }
 
-                    for (int j = 0;j < subStrings.Length; j++)
+                if (data[i] == "-")
+                {
+                    string lines = sb.ToString();
+                    sb.Clear();
+
+                    string[] split = lines.Split(' ');
+
+                    if (split.Length == 8)
+                        validCount++;
+                    else if (split.Length == 7)
                     {
-                        string[] subStringsAgain = subStrings[j].Split(':');
-
-                        int indexOfField = Array.IndexOf(fields, subStringsAgain[0]);
-
-                        fieldsPresent[indexOfField] = true;
+                        string line = "";
+                        foreach (string s in split)
+                        {
+                            
+                        }
                     }
                 }
             }
 
             // 285 too high
+            // 200 too low
             Console.WriteLine(validCount);
         }
     }
