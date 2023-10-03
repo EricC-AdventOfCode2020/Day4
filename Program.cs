54a937783a3f81ced3bac36b0b12fb2a6c100835
Eric Comanescu <ericcom@outlook.com>
2023-10-03 13:27:22 -0400

    made something that somewhat works

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Day4
+﻿using System.Runtime.InteropServices;
+
+namespace Day4
 {
     internal class Program
     {
@@ -10,7 +12,7 @@
             {
                 while (!sr.EndOfStream)
                 {
-                    string line = sr.ReadLine();
+                    string line = sr.ReadLine()!;
 
                     if (line == "")
                         line = "-";
@@ -23,10 +25,43 @@
             // SOLUTION: made it so when reading file, if line == empty string, replace line with '-'
 
             // TODO: solve problem now
+            string[] fields = { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "cid", "hgt" };
+
+            bool[] fieldsPresent = { false, false, false, false, false, false, false, false };
+
+            int validCount = 0;
+
             for (int i = 0; i < data.Count; i++)
             {
+                string line = data[i];
 
+                if (line == "-")
+                {
+                    for (int j = 0; j < fields.Length; j++)
+                    {
+                        if (fieldsPresent[j] == false && fields[j] != "cid")
+                            break;
+                        else if (fieldsPresent[j] == true && j == fields.Length - 1)
+                            validCount++;
+                    }
+                }
+                else
+                {
+                    string[] subStrings = line.Split(' ');
+
+                    for (int j = 0;j < subStrings.Length; j++)
+                    {
+                        string[] subStringsAgain = subStrings[j].Split(':');
+
+                        int indexOfField = Array.IndexOf(fields, subStringsAgain[0]);
+
+                        fieldsPresent[indexOfField] = true;
+                    }
+                }
             }
+
+            // 285 too high
+            Console.WriteLine(validCount);
         }
     }
 }
