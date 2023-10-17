8b211ea8a3fc929765e3a9bbd6c4df8109d00ee9
Eric Comanescu <117883271+BreadBurgIar@users.noreply.github.com>
2023-10-17 15:13:06 -0400

    fuck
    fucker

--- Program.cs
+++ Program.cs
@@ -67,7 +67,7 @@ namespace Day4
 
                 if (theThing[i] == "-")
                 {
-                    Regex rx = new Regex(@"byr:\b(19[2-9][0-9]|200[0-2])\b|iyr:\b(201[0-9]|2020)\b|eyr:\b(202[0-9]|2030)\b|hgt:\b(15[0-9]cm|16[0-9]cm|17[0-9]cm|18[0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)\b|hcl:#[a-f0-9]{6}|ecl:\b(amb|blu|brn|gry|grn|hzl|oth)\b|pid:[0-9]{9}");
+                    Regex rx = new Regex(@"byr:\b(19[2-9][0-9]|200[0-2])\b|iyr:\b(201[0-9]|2020)\b|eyr:\b(202[0-9]|2030)\b|hgt:\b(15[0-9]cm|16[0-9]cm|17[0-9]cm|18[0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)\b|hcl:#[a-f0-9]{6}|ecl:\b(amb|blu|brn|gry|grn|hzl|oth)\b|pid:\b[0-9]{9}\b");
                     //Regex rx = new Regex(@"byr:\b19[2-9][0-9]|200[0-2]\b|iyr:\b201[0-9]|2020\b|eyr:\b202[0-9]|2030\b|hgt:\b15[0-9]cm|16[0-9]cm|17[0-9]cm|18[0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in\b|hcl:#[a-f0-9]{6}|\becl:amb|ecl:blu|ecl:brn|ecl:gry|ecl:grn|ecl:hzl|ecl:oth\b|pid:[0-9]{9}");
 
                     string line = sb.ToString();
